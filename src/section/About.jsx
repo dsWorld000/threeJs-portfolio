0497708c4c4fb6e1fd36ac17d4a6c313fd72f4a9
[dsWorld000] <[ds137958@gmail.com]>
2024-09-27 16:08:34 +0530

    btn update

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -70,7 +70,9 @@ const About = () => {
             <div>
               <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
               <p className="grid-subtext">I&apos;m based in Haryana, India and open to remote work worldwide.</p>
+              <a href="#contact">
               <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+              </a>
             </div>
           </div>
         </div>
